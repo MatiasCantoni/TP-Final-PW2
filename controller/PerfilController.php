934c10bd31f012e4590637bd25ead6c62ae05f95
Antonio Sandoval <sandoval.mas23@gmail.com>
2025-11-10 12:40:02 -0300

    Add files via upload

---
+++ controller/PerfilController.php
@@ -0,0 +1,58 @@
+<?php
+
+class PerfilController {
+    private $perfilModel;
+    private $rankingModel;
+    private $renderer;
+
+    public function __construct($perfilModel, $rankingModel, $renderer) {
+        $this->perfilModel = $perfilModel;
+        $this->rankingModel = $rankingModel;
+        $this->renderer = $renderer;
+    }
+
+    public function base() {
+        $this->show();
+    }
+    
+    public function show() {
+        if (!isset($_SESSION["usuario"])) {
+            header("Location: /user/loginForm");
+            exit();
+        }
+
+        $idUsuario = isset($_GET["id"]) ? (int)$_GET["id"] : $_SESSION["usuario"]["id_usuario"];
+        
+        $usuario = $this->perfilModel->getUsuarioById($idUsuario);
+        
+        if (!$usuario) {
+            header("Location: /ranking/");
+            exit();
+        }
+
+        $partidas = $this->perfilModel->getPartidasUsuario($idUsuario);
+        $estadisticas = $this->perfilModel->getEstadisticasUsuario($idUsuario);
+        
+        $posicion = $this->rankingModel->getPosicionUsuario($idUsuario);
+
+        $urlPerfil = "http://" . $_SERVER['HTTP_HOST'] . "/perfil/show?id=" . $idUsuario;
+
+        $esPerfilPropio = $idUsuario == $_SESSION["usuario"]["id_usuario"];
+        
+        if ($esPerfilPropio) {
+            $usuario['contrasena_oculta'] = str_repeat('*', 10);
+        }
+
+        $data = [
+            "isPerfil" => true,
+            "usuario" => $usuario,
+            "partidas" => $partidas,
+            "estadisticas" => $estadisticas,
+            "posicion" => $posicion,
+            "urlPerfil" => $urlPerfil,
+            "esPerfilPropio" => $esPerfilPropio
+        ];
+
+        $this->renderer->render("perfil", $data);
+    }
+}
--- controller/RankingController.php
+++ controller/RankingController.php
@@ -33,47 +33,4 @@ class RankingController {
 
         $this->renderer->render("ranking", $data);
     }
-
-    
-    public function perfil() {
-        if (!isset($_SESSION["usuario"])) {
-            header("Location: /user/loginForm");
-            exit();
-        }
-
-        $idUsuario = isset($_GET["id"]) ? (int)$_GET["id"] : $_SESSION["usuario"]["id_usuario"];
-        
-        $usuario = $this->model->getUsuarioById($idUsuario);
-        
-        if (!$usuario) {
-            header("Location: /ranking/index");
-            exit();
-        }
-
-        $partidas = $this->model->getPartidasUsuario($idUsuario);
-        
-        $estadisticas = $this->model->getEstadisticasUsuario($idUsuario);
-        
-        $posicion = $this->model->getPosicionUsuario($idUsuario);
-
-        $urlPerfil = "http://" . $_SERVER['HTTP_HOST'] . "/ranking/perfil?id=" . $idUsuario;
-
-        $esPerfilPropio = $idUsuario == $_SESSION["usuario"]["id_usuario"];
-        
-        if ($esPerfilPropio) {
-            $usuario['contrasena_oculta'] = str_repeat('*', 10);
-        }
-
-        $data = [
-            "isPerfil" => true,
-            "usuario" => $usuario,
-            "partidas" => $partidas,
-            "estadisticas" => $estadisticas,
-            "posicion" => $posicion,
-            "urlPerfil" => $urlPerfil,
-            "esPerfilPropio" => $esPerfilPropio
-        ];
-
-        $this->renderer->render("perfil", $data);
-    }
 }
