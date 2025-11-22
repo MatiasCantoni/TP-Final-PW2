83cfb5b9de2b51f88a31db3640ca011311b0607d
Antonio Sandoval <sandoval.mas23@gmail.com>
2025-11-21 23:52:16 -0300

    Corrección PerfilController

--- controller/PerfilController.php
+++ controller/PerfilController.php
@@ -16,6 +16,10 @@ class PerfilController {
     }
     
     public function show() {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        
         if (!isset($_SESSION["usuario"])) {
             header("Location: /user/loginForm");
             exit();
@@ -32,16 +36,19 @@ class PerfilController {
 
         $partidas = $this->perfilModel->getPartidasUsuario($idUsuario);
         $estadisticas = $this->perfilModel->getEstadisticasUsuario($idUsuario);
-        
         $posicion = $this->rankingModel->getPosicionUsuario($idUsuario);
-
-        $urlPerfil = "http://" . $_SERVER['HTTP_HOST'] . "/perfil/show?id=" . $idUsuario;
+        $host = $_SERVER['HTTP_HOST'] ?? 'localhost';
+        $urlPerfil = "http://" . $host . "/perfil/show?id=" . $idUsuario;
 
         $esPerfilPropio = $idUsuario == $_SESSION["usuario"]["id_usuario"];
         
         if ($esPerfilPropio) {
             $usuario['contrasena_oculta'] = str_repeat('*', 10);
         }
+
+        $sexoMasculino = $usuario['sexo'] == 'Masculino';
+        $sexoFemenino = $usuario['sexo'] == 'Femenino';
+        $sexoOtro = $usuario['sexo'] == 'Prefiero no cargarlo';
 
         $data = [
             "isPerfil" => true,
@@ -51,9 +58,37 @@ class PerfilController {
             "posicion" => $posicion,
             "urlPerfil" => $urlPerfil,
             "esPerfilPropio" => $esPerfilPropio,
-            "showNavbar" => true
+            "showNavbar" => true,
+            "sexoMasculino" => $sexoMasculino,
+            "sexoFemenino" => $sexoFemenino,
+            "sexoOtro" => $sexoOtro
         ];
 
         $this->renderer->render("perfil", $data);
+    }
+
+    public function actualizar() {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_SESSION['usuario'])) {
+            $id = $_POST['id_usuario'];
+            if ($id == $_SESSION['usuario']['id_usuario']) {
+                $this->perfilModel->actualizarUsuario(
+                    $id,
+                    $_POST['nombre_completo'],
+                    $_POST['anio_nacimiento'],
+                    $_POST['sexo'],
+                    $_POST['pais'],
+                    $_POST['ciudad']
+                );
+                $_SESSION['usuario']['nombre_completo'] = $_POST['nombre_completo'];
+                $_SESSION['usuario']['pais'] = $_POST['pais'];
+                $_SESSION['usuario']['ciudad'] = $_POST['ciudad'];
+            }
+        }
+        header("Location: /perfil/show");
+        exit();
     }
 }
